a75b9b46145fb9e299b8e417e3c7ae570616fa54
pxn626 <790864000@qq.com>
2020-11-12 10:52:18 +0800

    Update config.php

--- down/config.php
+++ down/config.php
@@ -1,15 +1,1 @@
-<?php
-//mysql database address
-define('DB_HOST','localhost');
-//mysql database user
-define('DB_USER','sfydb_6363076');
-//database password
-define('DB_PASSWD','6z68733B');
-//database name
-define('DB_NAME','sfydb_6363076');
-//database prefix
-define('DB_PREFIX','down10_');
-//auth key
-define('AUTH_KEY','7#EJ$vnE47rv!Md5aCe$rycsbf7zW#Uvb3526d24fd9fb59df9955b5e0f895777');
-//cookie name
-define('AUTH_COOKIE_NAME','EM_AUTHCOOKIE_Dyt6popA7J0LgM2KkBvai25R3Dn3VqTh');
+
